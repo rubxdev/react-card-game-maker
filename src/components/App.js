9bfc28f0693b70f6f018519be8f3f80c32573ba5
Rubén <52975208+rubxdev@users.noreply.github.com>
2019-10-04 19:18:11 +0100

    💄 Add styled-normalized

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import { Router } from '@reach/router';
 import React from 'react';
 import GameList from './GameList';
+import { Normalize } from 'styled-normalize';
 
 const App = () => {
   return (
     <>
+      <Normalize />
       <Router>
         <GameList default path={'/games'} />
       </Router>
